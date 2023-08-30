0b9aace2249d1b99a7ea97f60e119d228baff0ba
zsolt115 <sikozsolt96@gmail.com>
2023-08-30 14:28:34 +0300

    done, new filter for price

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,34 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from '../data/data.js'
 
 const Food = () => {
+    const [foods, setFoods] = useState(data)
+
+    // filter type burgers/pizza/etc
+    const filterType = (category) => {
+        setFoods(
+            data.filter((item) => {
+                return item.category === category;
+            })
+        );
+    }
+
+    const filterPrice = (min, max) => {
+        setFoods(
+            data.filter((item) => {
+                return item.price > min && item.price <= max;
+            })
+        );
+    }
+
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
-        <h1 className='text-green-600'>Top Rated Menu Items</h1>
+        <h1 className='text-green-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
 
         {/** filter row */}
-        <div>
+        <div className='flex flex-col lg:flex-row justify-between pt-10'>
             {/** filter type */}
             <div>
-                <p>Filter Type</p>
-                <div>
-                    <button>All</button>
-                    <button>Burgers</button>
-                    <button>Pizza</button>
-                    <button>Salads</button>
-                    <button>Chicken</button>
+                <p className='font-bold text-gray-700'>Filter Type</p>
+                <div className='flex justify-between flex-wrap'>
+                    <button onClick={() => setFoods(data)} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>All</button>
+                    <button onClick={() => filterType('burger')} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>Burgers</button>
+                    <button onClick={() => filterType('pizza')} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>Pizza</button>
+                    <button onClick={() => filterType('salad')} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>Salads</button>
+                    <button onClick={() => filterType('chicken')} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>Chicken</button>
                 </div>
             </div>
             {/** filter price */}
             <div>
-                <p>Filter Price</p>
-                <div>
-                    <button>$</button>
-                    <button>$$</button>
-                    <button>$$$</button>
-                    <button>$$$$</button>
+                <p className='font-bold text-gray-700'>Filter Price</p>
+                <div className='flex justify-between max-w-[690px] w-full'>
+                    <button onClick={() => filterPrice(0, 10)} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>Less than 10</button>
+                    <button onClick={() => filterPrice(10, 20)} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>Between 10-20</button>
+                    <button onClick={() => filterPrice(20, 30)} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>Between 20-30</button>
+                    <button onClick={() => filterPrice(30, 40)} className='m-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>More than 30</button>
                 </div>
             </div>
+        </div>
+
+{/** display foods */}
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+            {foods.map((item, index) => (
+                <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
+                    <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-t-lg'/>
+                    <div className='flex justify-between px-2 py-4'>
+                        <p className='font-bold'>{item.name}</p>
+                        <p>
+                            <span className='bg-green-500 text-white p-1 rounded-full'>{item.price}</span>
+                        </p>
+                    </div>
+                </div>
+            ))}
         </div>
     </div>
   )
